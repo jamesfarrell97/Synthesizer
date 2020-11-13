471033b0fa7d578ed27936793e7fe5d6009dff1d
James Farrell <D00206780@student.dkit.ie>
2020-11-13 16:48:15 +0000

    feat (ui): implement basic synth

--- Assets/Scripts/Synthesizer.cs
+++ Assets/Scripts/Synthesizer.cs
@@ -1,38 +1,153 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Synthesizer : MonoBehaviour
 {
     private CsoundUnity csoundUnity;
-    private float frequency = 1;
-    private float frequencyFactor = 10;
-    
+
+    #region Sine
+    private float sineAmplitude;
+    private float sineFrequency;
+    private int sine = -1;
+
+    public void SetSineAmplitude(Slider slider)
+    {
+        sineAmplitude = slider.value;
+        csoundUnity.setChannel("sineAmpl", sineAmplitude);
+        Debug.Log(sineAmplitude);
+    }
+
+    public void SetSineFrequency(Slider slider)
+    {
+        sineFrequency = slider.value;
+        csoundUnity.setChannel("sineFreq", sineFrequency);
+        Debug.Log(sineFrequency);
+    }
+
+    public void ToggleSine()
+    {
+        sine *= -1;
+        csoundUnity.setChannel("sine", sine);
+        Debug.Log(sine);
+
+    }
+    #endregion
+
+    #region Square
+    private float squareAmplitude;
+    private float squareFrequency;
+    private int square = -1;
+
+    public void SetSquareAmplitude(Slider slider)
+    {
+        squareAmplitude = slider.value;
+        csoundUnity.setChannel("squareAmpl", squareAmplitude);
+    }
+
+    public void SetSquareFrequency(Slider slider)
+    {
+        squareFrequency = slider.value;
+        csoundUnity.setChannel("squareFreq", squareFrequency);
+    }
+
+    public void ToggleSquare()
+    {
+        square *= -1;
+        csoundUnity.setChannel("square", square);
+    }
+    #endregion
+
+    #region Saw
+    private float sawAmplitude;
+    private float sawFrequency;
+    private int saw = -1;
+
+    public void SetSawAmplitude(Slider slider)
+    {
+        sawAmplitude = slider.value;
+        csoundUnity.setChannel("sawAmpl", sawAmplitude);
+    }
+
+    public void SetSawFrequency(Slider slider)
+    {
+        sawFrequency = slider.value;
+        csoundUnity.setChannel("sawFreq", sawFrequency);
+    }
+
+    public void ToggleSaw()
+    {
+        saw *= -1;
+        csoundUnity.setChannel("saw", saw);
+    }
+    #endregion
+
+    #region Triangle
+    private float triangleAmplitude;
+    private float triangleFrequency;
+    private int triangle = -1;
+
+    public void SetTriangleAmplitude(Slider slider)
+    {
+        triangleAmplitude = slider.value;
+        csoundUnity.setChannel("triangleAmpl", triangleAmplitude);
+    }
+
+    public void SetTriangleFrequency(Slider slider)
+    {
+        triangleFrequency = slider.value;
+        csoundUnity.setChannel("triangleFreq", triangleFrequency);
+    }
+
+    public void ToggleTriangle()
+    {
+        triangle *= -1;
+        csoundUnity.setChannel("triangle", triangle);
+    }
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
         csoundUnity = GetComponent<CsoundUnity>();
+
+        Debug.Log(sine);
+
+        csoundUnity.setChannel("sine", sine);
+        csoundUnity.setChannel("square", square);
+        csoundUnity.setChannel("saw", saw);
+        csoundUnity.setChannel("triangle", triangle);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            Debug.Log(frequency);
-            csoundUnity.setChannel("freq", frequency += frequencyFactor);
-        }
+        //if (sine)
+        //{
+        //    Debug.Log("F: " + sineFrequency);
+        //    Debug.Log("A: " + sineAmplitude);
+        //    csoundUnity.setChannel("sineFreq", sineFrequency);
+        //    csoundUnity.setChannel("sineAmpl", sineAmplitude);
+        //}
 
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            Debug.Log(frequency);
-            csoundUnity.setChannel("play", Random.Range(0, 1000));
-        }
+        //if (square)
+        //{
+        //    csoundUnity.setChannel("squareFreq", squareFrequency);
+        //    csoundUnity.setChannel("squareAmpl", squareAmplitude);
+        //}
 
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            Debug.Log(frequency);
-            csoundUnity.setChannel("stop", Random.Range(0, 1000));
-        }
+        //if (saw)
+        //{
+        //    csoundUnity.setChannel("sawFreq", sawFrequency);
+        //    csoundUnity.setChannel("sawAmpl", sawAmplitude);
+        //}
+
+        //if (triangle)
+        //{
+        //    csoundUnity.setChannel("triangleFreq", triangleFrequency);
+        //    csoundUnity.setChannel("triangleAmpl", triangleAmplitude);
+        //}
     }
 }
--- Assets/StreamingAssets/Synthesizer.csd_
+++ Assets/StreamingAssets/Synthesizer.csd_
@@ -7,47 +7,111 @@
 sr = 44100 
 ksmps = 32
 nchnls = 2
-0dbfs = 1 
- 
-instr INSTR
-    kPlay   init 0
+0dbfs = -1
 
-    kFreq   chnget "freq"                                                       ;chnget - Reads data from the software bus
-    kPan    chnget "panR"                                                       ;chnget - Reads data from the software bus
-    kPlay   chnget "play"                                                       ;chnget - Reads data from the software bus
-    kStop   chnget "stop"                                                       ;chnget - Reads data from the software bus
+// Sine
+giSine  ftgen   0, 0, 2^10, 10, 1
 
-    if (changed(kPlay) == 1) then
-        event "i", "NOTE", 0, 1, kFreq, kPan
+instr SYNTH
+    kTempo          chnget "tempo"
+
+    kSine           chnget "sine"
+    kSquare         chnget "square"
+    kSaw            chnget "saw"
+    kTriangle       chnget "triangle"
+
+    kSineFreq       chnget "sineFreq"
+    kSquareFreq     chnget "squareFreq"
+    kSawFreq        chnget "sawFreq"
+    kTriangleFreq   chnget "triangleFreq"
+
+    kSineAmpl       chnget "sineAmpl"
+    kSquareAmpl     chnget "squareAmpl"
+    kSawAmpl        chnget "sawAmpl"
+    kTriangleAmpl   chnget "triangleAmpl"
+
+    ; while kSine == 1 do
+    ;     event "i", "SINE", 0, 1, kSineFreq, kSineAmpl
+    ; od
+
+    if (changed(kSine) == 1) then
+        event "i", "SINE", 0, 1, kSineFreq, kSineAmpl
     endif
 
-    if (changed(kStop) == 1) then
-        event "i", "NOTE", 0, 0
+    if (changed(kSquare) == 1) then
+        event "i", "SQUARE", 0, 1, kSquareFreq, kSquareAmpl
+    endif
+
+    if (changed(kSaw) == 1) then
+        event "i", "SAW", 0, 1, kSawFreq, kSawAmpl
+    endif
+    
+    if (changed(kTriangle) == 1) then
+        event "i", "TRIANGLE", 0, 1, kTriangleFreq, kTriangleAmpl
     endif
 endin
 
-instr NOTE
+instr SINE
+    // Parameters
     kFreq = p4
-    kPan = p5
-
-    iAtt = 0.1 ;Raise Sound
-    iDec = 0.5 ;Lower Sound
-    iSus = 0.3 ;Sustain Sound
-    iRel = 0.2 ;Release Sound
-
-    //Create envelope
-    kEnv    madsr iAtt, iDec, iSus, iRel
-
-    a1      inch 1                                                                  ;inch   - Reads from numbered channels in an external audio signal or stream
-    a2      inch 2                                                                  ;inch   - Reads from numbered channels in an external audio signal or stream
-    aComb   comb a1, 2, .2                                                          ;comb   - Reverberates an input signal with a “colored” frequency response
-            outs oscil:a(1 - kPan, kFreq) * kEnv, oscil:a(1 - kPan, kFreq) * kEnv   ;oscil  - A simple oscillator ;outs - Writes audio data to an external device or stream
+    kAmpl = p5
+    
+    // Create initial wave
+    aSinO   poscil kAmpl, kFreq, giSine              ;aSin Output
+    
+    outs    aSinO, aSinO
 endin
 
+instr SQUARE
+    // Parameters
+    kFreq = p4
+    kAmpl = p5
+    
+    // Create initial wave
+    aSinO   poscil kAmpl, kFreq, giSine              ;aSin Output
+    
+    outs    aSinO, aSinO
+endin
+
+instr SAW
+    // Parameters
+    kFreq = p4
+    kAmpl = p5
+    
+    // Create initial wave
+    aSinO   poscil kAmpl, kFreq, giSine              ;aSin Output
+    
+    outs    aSinO, aSinO
+endin
+
+instr TRIANGLE
+    // Parameters
+    kFreq = p4
+    kAmpl = p5
+    
+    // Create initial wave
+    aSinO   poscil kAmpl, kFreq, giSine              ;aSin Output
+    
+    outs    aSinO, aSinO
+endin
+
+; instr ADD
+;     // Create initial wave
+;     aSinO   poscil kAmp, kFreq, giSine              ;aSin Output
+
+;     kIndx = 1
+
+;     loop:
+;     aSinL   poscil kAmp / kIndx, kFreq * kIndx, giSine          ;aSin Loop
+;     aSinO = aSinO + aSinL
+;     loop_lt kIndx, kInc, kInc * kItr, loop
+
+;     // Stereo output
+;     outs    aSinO, aSinO
+; endin
 </CsInstruments>
 
 <CsScore>
-;i        p2           p3
-i"INSTR"   0  [3000 * 10]
+i"SYNTH"   0  [3600 * 12]
 </CsScore>
 </CsoundSynthesizer>
